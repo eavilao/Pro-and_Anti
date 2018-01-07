82d797edcd5c09769389d468f3f3dfb937329e29
Eric <eavilao@gmail.com>
2018-01-07 11:14:10 -0600

    eye kinematics+plot

--- eyeKinematics_ProAnti.m
+++ eyeKinematics_ProAnti.m
@@ -1,3 +1,4 @@
+function eyeKin = eyeKinematics_ProAnti(trialData)
 % Saccade kinematics
 
 %IN = trialData.m
@@ -11,12 +12,15 @@ for cellNum = 1:length(trialData)
         eyeKin(1,cellNum).proAmp(trialNum,1) = trialData(cellNum).pro.trial(trialNum).saccAmplitude;
         eyeKin(1,cellNum).proDur(trialNum,1) = trialData(cellNum).pro.trial(trialNum).saccDuration;
         eyeKin(1,cellNum).proPV(trialNum,1) = trialData(cellNum).pro.trial(trialNum).saccPeakVel;
+        eyeKin(1,cellNum).proRT(trialNum,1) = trialData(cellNum).pro.trial(trialNum).reactionTime;
+        
     end
     %antisacc
      for trialNum = 1:length([trialData(cellNum).anti.trial.saccAmplitude])
         eyeKin(1,cellNum).antiAmp(trialNum,1) = trialData(cellNum).anti.trial(trialNum).saccAmplitude;
         eyeKin(1,cellNum).antiDur(trialNum,1) = trialData(cellNum).anti.trial(trialNum).saccDuration;
         eyeKin(1,cellNum).antiPV(trialNum,1) = trialData(cellNum).anti.trial(trialNum).saccPeakVel;
+        eyeKin(1,cellNum).antiRT(trialNum,1) = trialData(cellNum).anti.trial(trialNum).reactionTime;
      end
 end
 
@@ -28,6 +32,12 @@ antiDur = vertcat(eyeKin(1,:).antiDur);
 
 proPV = vertcat(eyeKin(1,:).proPV); 
 antiPV = vertcat(eyeKin(1,:).antiPV); 
+
+proRT = vertcat(eyeKin(1,:).proRT)*1000; 
+antiRT = vertcat(eyeKin(1,:).antiRT)*1000; 
+
+meanProAmp = mean(proAmp); 
+meanAntiAmp = mean (antiAmp); 
 
 %% plot
 
@@ -69,3 +79,18 @@ set(h1(1),'FaceColor', [1 0 0], 'EdgeColor', [1 0 0]);
 set(h2(1),'FaceColor', [0 1 0],'EdgeColor', [0 1 0]);
 set(h1(2),'Color',[1 0 0]);
 set(h2(2),'Color',[0 1 0]);
+
+% plot RT
+figure; hold on
+h1 = histfit(proRT,20,'kernel');
+h2 = histfit(antiRT,20,'kernel');
+xlabel('Reaction time (ms)')
+ylabel('Number of trials')
+set (gca, 'TickDir', 'out','FontSize', 18);
+alpha(0.25)
+set(h1(1),'FaceColor', [1 0 0], 'EdgeColor', [1 0 0]);
+set(h2(1),'FaceColor', [0 1 0],'EdgeColor', [0 1 0]);
+set(h1(2),'Color',[1 0 0]);
+set(h2(2),'Color',[0 1 0]);
+
+
