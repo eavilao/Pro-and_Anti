b61190bd45cadfb6b43638a8f36385c65d16abb5
HERSENINSTITUUT\flierman <nicoflierman@gmail.com>
2018-02-08 15:09:00 +0100

    locating files containing spikes and stimlists

--- buildRecordingList.m
+++ buildRecordingList.m
@@ -46,7 +46,7 @@ for i= 1:size(cell_names,1)
     
     % path and name of cells
     neuronList(i).path = cell_folders{i};
-    neuronList(i).name = cell_names{i};
+    neuronList(i).neuronName = cell_names{i};
     
     % detect area from path string
     if regexpi(cell_folder{i},'Vermis')
@@ -67,11 +67,124 @@ for i= 1:size(cell_names,1)
         disp('no monkey name detected')
         keyboard
     end
+
+    
+    % now get the date of the recording
+  year = regexp(cell_names{i},'1+[234567]+'); % 2012 - 2017
+    day = regexp(cell_names{i},'[123]+[0123456789]+') ;% 1 - 31
+    daystring = cell_names{i}(year(1):day(end)+1);
+    recDay = datetime(daystring,'InputFormat','yy_MM_dd');
+    neuronList(i).recordingDate = recDay;
+    
+    % compare recording date with newer (nico) recordings, these have
+    % eyechannels on different input. Newer than 2016-01-01 
+    if recDay > datetime('16_01_01','InputFormat','yy_MM_dd')
+        neuronList(i).eyeChannels = [1,2];
+    else 
+        neuronList(i).eyeChannels = [];
+    end
         
-        
+    if exist( cell_folders{i})==7
+    recordings = getAllFiles(cell_folders{i});
+    elseif exist([cell_folders{i},'ProAndAnti'])
+    recordings =   getAllFiles([cell_folders{i},'ProAndAnti']);
+    else
+        disp('cant locate folder:')
+        disp(cell_folders{i})
+        keyboard
+    end
+    
+    % find all rsd files
+     rsdfiles = recordings;
+     matfiles = recordings;
+     matfiles(cellfun(@isempty, regexpi(matfiles,'mat')))=[];
+     rsdfiles(cellfun(@isempty, regexpi(rsdfiles,'rsd')))=[];
+     
+     if isempty(rsdfiles)
+         % no rsd files throw neuron from neuronList - add to error list 
+         disp('cell not spike-sorted')
+         error{i,1}= cell_folders{i};
+         error{i,2}= 'not sorted';
+         
+         neuronList(i)=[];
+         continue
+     end
+     
+     % get recording channel number from .rsd string
+     str_rsd = cell2mat(regexpi(rsdfiles(1),'.rsd'));
+     recording_channel = str2num(rsdfiles{1}(str_rsd-2:str_rsd));
+     
+     for ii = 1:size(rsdfiles,1)
+         rsd{ii,:}=rsdfiles{ii}(1:end-7);
+     end
+     for ii = 1:size(matfiles,1)
+         mat{ii,:}=matfiles{ii}(1:end-4);
+     end
+
+     
+     for ii = 1:size(rsd,1)
        
+         % indexes of rsd files  in matfile-list e.g. ind == 1 matfile(1) == rsd 1 
+         ind(ii)  = find(strcmp(mat,rsd{ii}));
+%         
+     end
+      
+     % slightly messy indexing 
+     index = zeros(size(mat,1),1);
+     index(ind)=1;
+     
+     
+     spikeMatFiles = mat(ind);
+     otherMatFiles =  mat(index==0);
+     
+     % search non-spike matfiles for stimList
+     if ~isempty(otherMatFiles)
+         for ii= i:size(otherMatFiles,1)
+             
+             % get variables that are present in remaining mat files 
+             thisFile = matfile(otherMatFiles{ii});
+             thisFileInfo = whos(thisFile);
+             variablenames =  [thisFileInfo(:).name];
+                          
+             out = regexpi(variablenames,{'ans','epd','stimlist'});
+             
+             if cell2mat(out) && isempty( out{1}) % ans could potential be in a string due to coincidence
+                 otherMatFiles{ii,2} = 'stimListFile';
+                 stimListInfo = dir([otherMatFiles{ii,1},'.mat']); 
+                 otherMatFiles{ii,3} = stimListInfo.bytes; % add filesize to later find largest stimList(with most trials)
+             elseif ~isempty(out{1})  && thisFileInfo.name =='ans'
+                 
+                 
+                 otherMatFiles{ii,2} = 'stimListFile'; 
+                 stimListInfo = dir([otherMatFiles{ii,1},'.mat']);
+                 otherMatFiles{ii,3} = stimListInfo.bytes;
+             end
+         end
+     else
+         look for stimlist in stimlistfolder
+         
+         
+     end
+    take rsd - find matching mat files - these are the spike files. 
+    
+     any leftover files are likely the stimlistfile -> check by seeing if it contains variable: epd or stimlistfile or ans. 
+    find biggest stimlist file in mb's 
+    
 end
 
+
+eyeChannels = [1 2] % if recording newer then 2016
+
+% neuronList(39).neuronName = '17_01_20_N2';    % 
+% neuronList(39).fileList = {
+% % File Name                 spk channel number  [unit numbers] startTime stopTime stimStartFile resortFlag
+% 'F170120-0005.mat'          6              [1 2]     []          []      NaN    0
+% };
+% neuronList(39).group = 'Pause_Burst'; %not real 
+% neuronList(39).area = 'vermis';
+% neuronList(39).depth = 31.6; % real
+% neuronList(39).gridLocation = [23 5]; %real
+% neuronList(39).eyeChannels = [1 2];
 
 % get all files that end in [_number].txt 
 % folders3 = regexp(allfiles(:,1),'_[123456789]+.txt');
